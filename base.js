b8c988157ccc1687648a74ca7069a9f23662251d
Electrol <ftotsuji@gmail.com>
2024-08-12 18:21:48 +0900

    Added Discord button

--- base.js
+++ base.js
@@ -1,3 +1,23 @@
-document.getElementById("facebook-button").onclick = function() {
-    alert('messaj')
-};
+function openFacebook() {
+    window.open("https://facebook.com/electrolmusic");
+}
+
+function openSoundCloud() {
+    window.open("https://soundcloud.com/electrolmusic");
+}
+
+function openYouTube() {
+    window.open("https://youtube.com/@ElectrolOfficial");
+}
+
+function openInstagram() {
+    window.open("https://instagram.com/electrolmusic");
+}
+
+function openTwitter() {
+    window.open("https://twitter.com/Electrol_music");
+}
+
+function openDiscord() {
+    window.open("https://discord.gg/KFDE6ujHSE");
+}
